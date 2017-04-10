f67565fdd3773e53f2b6a0eed1fad09ff9d866ff
Jai Tiwari <contactjaitiwari@gmail.com>
2017-04-09 23:12:33 -0400

    final

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var path = require('path');
 var express = require('express');
+var request = require('request');
 var multer = require('multer');
+var upload = multer({ dest: 'upload/' });
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(process.env.PORT || 3000);
+app.post('/upload', upload.single("upl"), function (req, res, next) {
+  var return_obj = {
+    "size": req.file["size"]
+  }
+  res.send(return_obj);
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("listening to " + process.env.PORT || 3000));
